f36fd5ed8bc6e7ce311a293fcbfffe6b6381d483
Dao Xuan Quy <quydxhe176351@fpt.edu.vn>
2024-02-22 17:03:39 +0700

    fix: upload image

--- ckeditor5/sample/script.js
+++ ckeditor5/sample/script.js
@@ -12,7 +12,7 @@ function handleSampleError( error ) {
 
 	const message = [
 		'Oops, something went wrong!',
-		`Please, report the following error on ${ issueUrl } with the build id "2c549gfhjped-nohdljl880ze" and the error stack trace:`
+		`Please, report the following error on ${ issueUrl } with the build id "8ne6bitsobkg-nohdljl880ze" and the error stack trace:`
 	].join( '\n' );
 
 	console.error( message );
--- ckeditor5/src/ckeditor.ts
+++ ckeditor5/src/ckeditor.ts
@@ -27,6 +27,7 @@ import { PasteFromOffice } from '@ckeditor/ckeditor5-paste-from-office';
 import { Table, TableToolbar } from '@ckeditor/ckeditor5-table';
 import { TextTransformation } from '@ckeditor/ckeditor5-typing';
 import { Undo } from '@ckeditor/ckeditor5-undo';
+import { Base64UploadAdapter } from '@ckeditor/ckeditor5-upload';
 
 // You can read more about extending the build with additional plugins in the "Installing plugins" guide.
 // See https://ckeditor.com/docs/ckeditor5/latest/installation/plugins/installing-plugins.html for details.
@@ -34,6 +35,7 @@ import { Undo } from '@ckeditor/ckeditor5-undo';
 class Editor extends ClassicEditor {
 	public static override builtinPlugins = [
 		Autoformat,
+		Base64UploadAdapter,
 		BlockQuote,
 		Bold,
 		Essentials,

7ebba8ba635170246d68f611882f89cfdcc154a6
Dao Xuan Quy <quydxhe176351@fpt.edu.vn>
2024-02-29 05:34:13 +0700

    fix: service page

--- src/pages/services/index.tsx
+++ src/pages/services/index.tsx
@@ -1,189 +1,146 @@
-import { Button, Breadcrumb } from "@/components/common"
+import { Breadcrumb } from "@/components/common"
 import MainLayout from "@/components/layouts/MainLayout"
-import { TableSkeleton } from "@/components/skeletons"
-import withAuth from "@/hoc/withAuth"
-import type { Service } from "@/types"
-import { convertDate, instance } from "@/utils"
+import { Accordion } from "@mantine/core"
 import Link from "next/link"
-import { useEffect, useState } from "react"
-import {
-    MdOutlineDelete,
-    MdOutlineEdit,
-    MdOutlineRemoveRedEye,
-} from "react-icons/md"
-import Swal from "sweetalert2"
+import React from "react"
 
-const PAGE_SIZE = 500
+const services = [
+    {
+        value: "Phòng khám đa khoa",
+        data: [
+            {
+                description: "Đầu tư hạ tầng",
+                id: 2,
+            },
+            {
+                description: "Tư vấn vận hành",
+                id: 3,
+            },
+            {
+                description: "Tư vấn pháp lý",
+                id: 4,
+            },
+            {
+                description: "Tư vấn nhân sự",
+                id: 5,
+            },
+            {
+                description: "Công nghệ thông tin",
+                id: 6,
+            },
+        ],
+    },
+    {
+        value: "Phòng khám chuyên khoa",
+        data: [
+            {
+                description: "Đầu tư hạ tầng",
+                id: 7,
+            },
+            {
+                description: "Tư vấn vận hành",
+                id: 8,
+            },
+            {
+                description: "Tư vấn pháp lý",
+                id: 9,
+            },
+            {
+                description: "Tư vấn nhân sự",
+                id: 10,
+            },
+            {
+                description: "Công nghệ thông tin",
+                id: 11,
+            },
+        ],
+    },
+    {
+        value: "Viện dưỡng lão",
+        data: [
+            {
+                description: "Đầu tư hạ tầng",
+                id: 12,
+            },
+            {
+                description: "Tư vấn vận hành",
+                id: 13,
+            },
+            {
+                description: "Tư vấn pháp lý",
+                id: 14,
+            },
+            {
+                description: "Tư vấn nhân sự",
+                id: 15,
+            },
+            {
+                description: "Công nghệ thông tin",
+                id: 16,
+            },
+        ],
+    },
+    {
+        value: "Bệnh viện",
+        data: [
+            {
+                description: "Đầu tư hạ tầng",
+                id: 17,
+            },
+            {
+                description: "Tư vấn vận hành",
+                id: 18,
+            },
+            {
+                description: "Tư vấn pháp lý",
+                id: 19,
+            },
+            {
+                description: "Tư vấn nhân sự",
+                id: 20,
+            },
+            {
+                description: "Công nghệ thông tin",
+                id: 21,
+            },
+        ],
+    },
+]
 
-const Services = () => {
-    const [data, setData] = useState<Service[] | null>(null)
-
-    useEffect(() => {
-        instance
-            .get(`/services?perPage=${PAGE_SIZE}&order=desc`)
-            .then((res) => {
-                setData(res.data)
-            })
-    }, [])
-
-    const deleteService = (id: number) => {
-        instance
-            .delete(`/services/${id}`)
-            .then(() => {
-                const filteredTableData = data!.filter((item) => item.id !== id)
-                setData(filteredTableData)
-            })
-            .catch((err) => {
-                if (err.response.status === 401) {
-                    window.location.href = "/signin"
-                }
-            })
-    }
-
-    const handleDelete = (id: number) => {
-        Swal.fire({
-            title: "Bạn có chắc chắn muốn xóa?",
-            text: "Hành động này không thể hoàn tác!",
-            icon: "warning",
-            showCancelButton: true,
-            confirmButtonColor: "#3085d6",
-            cancelButtonColor: "#d33",
-            confirmButtonText: "Chắc chắn!",
-            cancelButtonText: "Hủy",
-        }).then((result) => {
-            if (result.isConfirmed) {
-                deleteService(id)
-                Swal.fire({
-                    title: "Đã xóa!",
-                    icon: "success",
-                })
-            }
-        })
-    }
-
+const ServicePage = () => {
     return (
         <MainLayout>
-            <Breadcrumb pageName="Dịch vụ" link="">
-                <Button
-                    color="success"
-                    variant="rounded"
-                    size="large"
-                    href="/services/create"
-                >
-                    Thêm mới
-                </Button>
-            </Breadcrumb>
-            {!data ? (
-                <TableSkeleton rows={5} columns={3} />
-            ) : (
-                <div className="rounded-sm border border-stroke bg-white px-5 pt-6 pb-2.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
-                    <div className="max-w-full overflow-x-auto">
-                        <table className="w-full table-auto">
-                            <thead>
-                                <tr className="bg-gray-2 text-left dark:bg-meta-4">
-                                    <th className="min-w-[220px] py-4 px-4 font-medium text-black dark:text-white xl:pl-11">
-                                        Tên
-                                    </th>
-                                    <th className="min-w-[220px] py-4 px-4 font-medium text-black dark:text-white xl:pl-11">
-                                        Ảnh nổi bật
-                                    </th>
-                                    <th className="py-4 px-4 font-medium text-black dark:text-white">
-                                        Mô tả
-                                    </th>
-                                    <th className="py-4 px-4 font-medium text-black dark:text-white">
-                                        Ngày tạo
-                                    </th>
-                                    <th className="py-4 px-4 font-medium text-black dark:text-white">
-                                        Ngày chỉnh sửa
-                                    </th>
-                                    <th className="py-4 px-4 font-medium text-black dark:text-white" />
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {data.length === 0 ? (
-                                    <tr>
-                                        <td colSpan={10}>
-                                            <div className="text-black/70 dark:text-white/70 w-full flex items-center justify-center h-20 font-medium">
-                                                Không có dữ liệu
-                                            </div>
-                                        </td>
-                                    </tr>
-                                ) : (
-                                    data.map(
-                                        ({
-                                            id,
-                                            name,
-                                            featuredImage,
-                                            description,
-                                            createdAt,
-                                            modifiedAt,
-                                        }) => (
-                                            <tr key={id}>
-                                                <td className="border-b border-[#eee] py-5 px-4 pl-9 dark:border-strokedark xl:pl-11">
-                                                    <h5 className="font-medium text-black dark:text-white">
-                                                        {name}
-                                                    </h5>
-                                                </td>
-                                                <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
-                                                    <div className="font-medium text-black dark:text-white">
-                                                        <img
-                                                            src={featuredImage}
-                                                            alt="featured image"
-                                                            className="h-40 object-cover object-center rounded-sm w-full"
-                                                        />
-                                                    </div>
-                                                </td>
-                                                <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
-                                                    <p className="text-black dark:text-white">
-                                                        {description}
-                                                    </p>
-                                                </td>
-                                                <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
-                                                    <p className="text-black dark:text-white">
-                                                        {convertDate(createdAt)}
-                                                    </p>
-                                                </td>
-                                                <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
-                                                    <p className="text-black dark:text-white">
-                                                        {convertDate(
-                                                            modifiedAt
-                                                        )}
-                                                    </p>
-                                                </td>
-                                                <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
-                                                    <div className="flex items-center space-x-3.5">
-                                                        <Link
-                                                            href={`/services/${id}`}
-                                                            className="hover:text-primary"
-                                                        >
-                                                            <MdOutlineRemoveRedEye className="text-xl" />
-                                                        </Link>
-                                                        <Link
-                                                            href={`/services/edit/${id}`}
-                                                            className="hover:text-success"
-                                                        >
-                                                            <MdOutlineEdit className="text-xl" />
-                                                        </Link>
-                                                        <button
-                                                            onClick={() =>
-                                                                handleDelete(id)
-                                                            }
-                                                            className="hover:text-danger"
-                                                        >
-                                                            <MdOutlineDelete className="text-xl" />
-                                                        </button>
-                                                    </div>
-                                                </td>
-                                            </tr>
-                                        )
-                                    )
-                                )}
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            )}
+            <Breadcrumb pageName="Dịch vụ" link=""></Breadcrumb>
+
+            <div className="rounded-sm border border-stroke bg-white p-6 shadow-default dark:border-strokedark dark:bg-boxdark">
+                <Accordion variant="separated">
+                    {services.map((service) => (
+                        <Accordion.Item
+                            key={service.value}
+                            value={service.value}
+                        >
+                            <Accordion.Control icon={<></>}>
+                                {service.value}
+                            </Accordion.Control>
+                            <Accordion.Panel>
+                                <div className="flex flex-col gap-2">
+                                    {service.data.map((data) => (
+                                        <Link
+                                            key={data.id}
+                                            href={`/services/edit/${data.id}`}
+                                            className="ml-5 text-black"
+                                        >
+                                            {data.description}
+                                        </Link>
+                                    ))}
+                                </div>
+                            </Accordion.Panel>
+                        </Accordion.Item>
+                    ))}
+                </Accordion>
+            </div>
         </MainLayout>
     )
 }
-export default withAuth(Services)
+
+export default ServicePage

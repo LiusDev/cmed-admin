69fecc24236083916d8100ec961756b7070a7d98
rezarria <votanamcb@outlook.com>
2024-04-09 12:27:18 +0700

    Add NumberInput component

--- src/pages/documents/create.tsx
+++ src/pages/documents/create.tsx
@@ -3,21 +3,42 @@ import MainLayout from "@/components/layouts/MainLayout"
 import { TableSkeleton } from "@/components/skeletons"
 import withAuth from "@/hoc/withAuth"
 import { Category } from "@/types"
-import { convertBase64, instance } from "@/utils"
-import { NumberInput } from "@mantine/core"
+import { alias, convertBase64, instance, langOptions } from "@/utils"
+import { SegmentedControl } from "@mantine/core"
+import { useForm } from "@mantine/form"
 import { useRouter } from "next/router"
-import { useCallback, useEffect, useState } from "react"
+import { useCallback, useEffect, useMemo, useState } from "react"
+import { TextInput } from "../../components/Text"
+import { NumberInput } from "../../components/NumberInput"
+import ImageInput from "../../components/ImageInput"
 
 const Create = () => {
+    const [lang, setLang] = useState<keyof typeof alias>("");
+    const currentAlias = useMemo(() => alias[lang], [lang]);
+    const form = useForm({
+        initialValues: {
+            name: "",
+            nameJP: "",
+            nameEN: "",
+            description: "",
+            descriptionJP: "",
+            descriptionEN: "",
+            categoryId: 0,
+            view: 0,
+            download: 0,
+            featuredImage: "",
+        }
+    })
     const [categories, setCategories] = useState<Category[] | null>(null)
-    const [name, setName] = useState("")
     const [category, setCategory] = useState<Category["id"]>()
-    const [description, setDescription] = useState("")
-    const [featuredImage, setFeaturedImage] = useState("")
-    const [view, setView] = useState(0)
-    const [download, setDownload] = useState(0)
-    const [document, setDocument] = useState<File | null>(null)
     const [loading, setLoading] = useState(false)
+    const [document, setDocument] = useState<File | null>(null)
+    const handleUploadDocument = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+        if (e.target.files) {
+            const file = e.target.files[0]
+            setDocument(file)
+        }
+    }, [])
 
     useEffect(() => {
         instance
@@ -33,56 +54,13 @@ const Create = () => {
             })
     }, [])
 
-    const handleChangeName = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-        setName(e.target.value)
-    }, [])
+
 
     const handleChangeCategory = useCallback((e: React.ChangeEvent<HTMLSelectElement>) => {
         setCategory(parseInt(e.target.value))
     }
         , [])
-    const handleChangeDescription = useCallback((
-        e: React.ChangeEvent<HTMLInputElement>
-    ) => {
-        setDescription(e.target.value)
-    }, [])
 
-    const handleUploadFeaturedImage = useCallback(async (
-        e: React.ChangeEvent<HTMLInputElement>
-    ) => {
-        if (e.target.files) {
-            const file = e.target.files[0]
-            const base64image = await convertBase64(file)
-            setFeaturedImage(base64image)
-        }
-    }, [])
-
-    const handleUploadDocument = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-        if (e.target.files) {
-            const file = e.target.files[0]
-            setDocument(file)
-        }
-    }, [])
-
-    const handleView = useCallback((value: string | number) => {
-        if (typeof value === "string") {
-            const numberValue = parseInt(value);
-            setView(numberValue);
-        }
-        else if (typeof value === "number") {
-            setView(value);
-        }
-    }, [])
-
-    const handleDownload = useCallback((value: string | number) => {
-        if (typeof value === "string") {
-            const numberValue = parseInt(value);
-            setDownload(numberValue);
-        }
-        else if (typeof value === "number") {
-            setDownload(value);
-        }
-    }, [])
 
     const router = useRouter()
 
@@ -92,12 +70,8 @@ const Create = () => {
             .post(
                 "/documents",
                 {
-                    name,
-                    description,
-                    featuredImage,
+                    ...form.values,
                     document,
-                    view,
-                    download,
                     categoryId: category,
                 },
                 {
@@ -117,7 +91,7 @@ const Create = () => {
             .finally(() => {
                 setLoading(false)
             })
-    }, [name, description, featuredImage, document, category, view, download])
+    }, [form, category, document])
 
     return (
         <MainLayout>
@@ -136,95 +110,34 @@ const Create = () => {
                         </h3>
                     </div>
                     <div className="flex flex-col gap-5.5 p-6.5">
-                        <div className="grid grid-cols-5 gap-2">
-                            <div className="col-span-3">
-                                <label className="mb-3 block text-black dark:text-white">
-                                    Tên
-                                </label>
-                                <input
-                                    value={name}
-                                    onChange={handleChangeName}
-                                    type="text"
-                                    placeholder="Tên tài liệu"
-                                    className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
-                                />
-                            </div>
-                            <div className="col-span-3">
-                                <label className="mb-3 block text-black dark:text-white">
-                                    Lượt xem
-                                </label>
-                                <NumberInput
-                                    value={view}
-                                    onChange={handleView}
-                                    type="text"
-                                    placeholder="Lượt xem"
-                                    className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
-                                />
-                            </div>
-                            <div className="col-span-3">
-                                <label className="mb-3 block text-black dark:text-white">
-                                    Lượt tải xuống
-                                </label>
-                                <NumberInput
-                                    value={download}
-                                    onChange={handleDownload}
-                                    type="text"
-                                    placeholder="Lượt tải xuống"
-                                    className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
-                                />
-                            </div>
-                            <div className="col-span-2">
-                                <label className="mb-3 block text-black dark:text-white">
-                                    Danh mục
-                                </label>
-                                <select
-                                    title="category"
-                                    name="category"
-                                    onChange={handleChangeCategory}
-                                    className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
-                                >
-                                    {categories.map((category) => (
-                                        <option
-                                            key={category.id}
-                                            value={category.id}
-                                        >
-                                            {category.name}
-                                        </option>
-                                    ))}
-                                </select>
-                            </div>
-                        </div>
-                        <div>
+                        <SegmentedControl disabled={loading} data={langOptions} value={lang} onChange={setLang as any} />
+                        <TextInput title={`Tên nhân viên ${currentAlias}`} {...form.getInputProps(`name${lang}`)} />
+                        <NumberInput title="Lượt xem" {...form.getInputProps("view")} />
+                        <NumberInput title="Lượt tải xuống" {...form.getInputProps("download")} />
+
+                        <div className="col-span-2">
                             <label className="mb-3 block text-black dark:text-white">
-                                Mô tả
+                                Danh mục
                             </label>
-                            <input
-                                value={description}
-                                onChange={handleChangeDescription}
-                                type="text"
-                                placeholder="Mô tả tài liệu"
+                            <select
+                                title="category"
+                                name="category"
+                                onChange={handleChangeCategory}
                                 className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
-                            />
+                            >
+                                {categories.map((category) => (
+                                    <option
+                                        key={category.id}
+                                        value={category.id}
+                                    >
+                                        {category.name}
+                                    </option>
+                                ))}
+                            </select>
                         </div>
-                        <div>
-                            <label className="mb-3 block text-black dark:text-white">
-                                Ảnh nổi bật
-                            </label>
-                            <input
-                                title="featuredImage"
-                                type="file"
-                                accept="image/*"
-                                onChange={handleUploadFeaturedImage}
-                                className="mb-3 w-full cursor-pointer rounded-lg border-[1.5px] border-stroke bg-transparent font-medium outline-none transition file:mr-5 file:border-collapse file:cursor-pointer file:border-0 file:border-r file:border-solid file:border-stroke file:bg-whiter file:py-3 file:px-5 file:hover:bg-primary file:hover:bg-opacity-10 focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:file:border-form-strokedark dark:file:bg-white/30 dark:file:text-white dark:focus:border-primary"
-                            />
-                            {featuredImage && (
-                                <img
-                                    src={featuredImage}
-                                    alt="featured image"
-                                    className="h-40 object-cover rounded-sm"
-                                />
-                            )}
-                        </div>
+                        <TextInput title={`Mô tả ${currentAlias}`} {...form.getInputProps(`description${lang}`)} />
+                        <ImageInput title="Ảnh nổi bật" {...form.getInputProps("featuredImage")} />
+
                         <div>
                             <label className="mb-3 block text-black dark:text-white">
                                 Upload tài liệu
--- src/pages/documents/edit/[id].tsx
+++ src/pages/documents/edit/[id].tsx
@@ -3,43 +3,39 @@ import MainLayout from "@/components/layouts/MainLayout"
 import { TableSkeleton } from "@/components/skeletons"
 import withAuth from "@/hoc/withAuth"
 import { Category, Document } from "@/types"
-import { convertBase64, instance } from "@/utils"
-import { NumberInput } from "@mantine/core"
+import { alias, instance, langOptions } from "@/utils"
+import { SegmentedControl } from "@mantine/core"
+import { useForm } from "@mantine/form"
 import { useRouter } from "next/router"
-import { useCallback, useEffect, useState } from "react"
+import { useCallback, useEffect, useMemo, useState } from "react"
+import ImageInput from "@/components/ImageInput"
+import { TextInput } from "@/components/Text"
+import { NumberInput } from "../../../components/NumberInput"
 
 const Create = () => {
+
+    const [lang, setLang] = useState<keyof typeof alias>("");
+    const currentAlias = useMemo(() => alias[lang], [lang]);
+    const form = useForm({
+        initialValues: {
+            name: "",
+            nameJP: "",
+            nameEN: "",
+            description: "",
+            descriptionJP: "",
+            descriptionEN: "",
+            categoryId: 0,
+            view: 0,
+            download: 0,
+            featuredImage: "",
+        }
+    })
+
     const [categories, setCategories] = useState<Category[] | null>(null)
     const [document, setDocument] = useState<Document | null>(null)
-    const [name, setName] = useState("")
     const [category, setCategory] = useState<Category["id"]>(1)
-    const [description, setDescription] = useState("")
-    const [featuredImage, setFeaturedImage] = useState("")
     const [documentUrl, setDocumentUrl] = useState<File | null>(null)
     const [loading, setLoading] = useState(false)
-
-    const [view, setView] = useState(0)
-    const [download, setDownload] = useState(0)
-
-    const handleView = useCallback((value: string | number) => {
-        if (typeof value === "string") {
-            const numberValue = parseInt(value);
-            setView(numberValue);
-        }
-        else if (typeof value === "number") {
-            setView(value);
-        }
-    }, [])
-
-    const handleDownload = useCallback((value: string | number) => {
-        if (typeof value === "string") {
-            const numberValue = parseInt(value);
-            setDownload(numberValue);
-        }
-        else if (typeof value === "number") {
-            setDownload(value);
-        }
-    }, [])
 
     let path: string
     useEffect(() => {
@@ -58,14 +54,8 @@ const Create = () => {
         instance
             .get(`/documents/${path}`)
             .then((res) => {
+                form.setValues(res.data)
                 setDocument(res.data)
-                setName(res.data.name)
-                setCategory(res.data.category.id)
-                setDescription(res.data.description)
-                setFeaturedImage(res.data.featuredImage)
-                setDocumentUrl(res.data.documentUrl)
-                setView(res.data.view)
-                setDownload(res.data.download)
             })
             .catch((err) => {
                 if (err.response.status === 401) {
@@ -74,25 +64,11 @@ const Create = () => {
             })
     }, [])
 
-    const handleChangeName = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-        setName(e.target.value)
-    }, [])
-
     const handleChangeCategory = useCallback((e: React.ChangeEvent<HTMLSelectElement>) => {
         setCategory(parseInt(e.target.value))
     }, [])
 
-    const handleChangeDescription = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-        setDescription(e.target.value)
-    }, [])
 
-    const handleUploadFeaturedImage = useCallback(async (e: React.ChangeEvent<HTMLInputElement>) => {
-        if (e.target.files) {
-            const file = e.target.files[0]
-            const base64image = await convertBase64(file)
-            setFeaturedImage(base64image)
-        }
-    }, [])
 
     const handleUploadDocument = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
         if (e.target.files) {
@@ -109,12 +85,9 @@ const Create = () => {
                 .patch(
                     `/documents/${document.id}`,
                     {
-                        name,
-                        description,
-                        featuredImage,
+                        ...form.values,
                         document: documentUrl,
                         categoryId: category,
-                        view, download
                     },
                     {
                         headers: {
@@ -155,94 +128,32 @@ const Create = () => {
                         </h3>
                     </div>
                     <div className="flex flex-col gap-5.5 p-6.5">
-                        <div className="grid grid-cols-5 gap-2">
-                            <div className="col-span-3">
-                                <label className="mb-3 block text-black dark:text-white">
-                                    Tên
-                                </label>
-                                <input
-                                    value={name}
-                                    onChange={handleChangeName}
-                                    type="text"
-                                    placeholder="Tên tài liệu"
-                                    className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
-                                />
-                            </div>
-                            <div className="col-span-3">
-                                <label className="mb-3 block text-black dark:text-white">
-                                    Lượt xem
-                                </label>
-                                <NumberInput
-                                    value={view}
-                                    onChange={handleView}
-                                    type="text"
-                                    placeholder="Lượt xem"
-                                    className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
-                                />
-                            </div>
-                            <div className="col-span-3">
-                                <label className="mb-3 block text-black dark:text-white">
-                                    Lượt tải xuống
-                                </label>
-                                <NumberInput
-                                    value={download}
-                                    onChange={handleDownload}
-                                    type="text"
-                                    placeholder="Lượt tải xuống"
-                                    className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
-                                />
-                            </div>
-                            <div className="col-span-2">
-                                <label className="mb-3 block text-black dark:text-white">
-                                    Danh mục
-                                </label>
-                                <select
-                                    title="Danh mục"
-                                    onChange={handleChangeCategory}
-                                    className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
-                                >
-                                    {categories.map((category) => (
-                                        <option
-                                            key={category.id}
-                                            value={category.id}
-                                        >
-                                            {category.name}
-                                        </option>
-                                    ))}
-                                </select>
-                            </div>
-                        </div>
-                        <div>
+                        <SegmentedControl disabled={loading} data={langOptions} value={lang} onChange={setLang as any} />
+                        <TextInput title={`Tên nhân viên ${currentAlias}`} {...form.getInputProps(`name${lang}`)} />
+                        <NumberInput title="Lượt xem" {...form.getInputProps("view")} />
+                        <NumberInput title="Lượt tải xuống" {...form.getInputProps("download")} />
+
+                        <div className="col-span-2">
                             <label className="mb-3 block text-black dark:text-white">
-                                Mô tả
+                                Danh mục
                             </label>
-                            <input
-                                value={description}
-                                onChange={handleChangeDescription}
-                                type="text"
-                                placeholder="Mô tả tài liệu"
+                            <select
+                                title="Danh mục"
+                                onChange={handleChangeCategory}
                                 className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
-                            />
+                            >
+                                {categories.map((category) => (
+                                    <option
+                                        key={category.id}
+                                        value={category.id}
+                                    >
+                                        {category.name}
+                                    </option>
+                                ))}
+                            </select>
                         </div>
-                        <div>
-                            <label className="mb-3 block text-black dark:text-white">
-                                Ảnh nổi bật
-                            </label>
-                            <input
-                                title="Ảnh nổi bật"
-                                type="file"
-                                accept="image/*"
-                                onChange={handleUploadFeaturedImage}
-                                className="mb-3 w-full cursor-pointer rounded-lg border-[1.5px] border-stroke bg-transparent font-medium outline-none transition file:mr-5 file:border-collapse file:cursor-pointer file:border-0 file:border-r file:border-solid file:border-stroke file:bg-whiter file:py-3 file:px-5 file:hover:bg-primary file:hover:bg-opacity-10 focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:file:border-form-strokedark dark:file:bg-white/30 dark:file:text-white dark:focus:border-primary"
-                            />
-                            {featuredImage && (
-                                <img
-                                    src={featuredImage}
-                                    alt="featured image"
-                                    className="h-40 object-cover rounded-sm"
-                                />
-                            )}
-                        </div>
+                        <TextInput title={`Mô tả ${currentAlias}`} {...form.getInputProps(`description${lang}`)} />
+                        <ImageInput title="Ảnh nổi bật" {...form.getInputProps("featuredImage")} />
                         <div>
                             <label className="mb-3 block text-black dark:text-white">
                                 Upload tài liệu
